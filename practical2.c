1b8b409abb3be3aa5a741a3c69ce3534a37806d2
mahirabari17 <25ce095@charusat.edu.in>
2025-11-20 12:15:14 +0530

    Add files via upload

---
+++ practical2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a=70;
+int b=90;
+int perimeter;
+int area;
+perimeter = 2*a+2*b;
+printf("perimeter of park is %d meter. \n",perimeter);
+area = a*b;
+printf("area of park: %d meter*meter.\n",area);
+return 0;
+}
